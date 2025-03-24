cc02def70bebd5637d7ca6f02b9a8b3f87c3718e
loucurnow <louie@learncoach.com>
2025-03-24 20:47:27 +1300

    renv

--- preprocess.R
+++ preprocess.R
@@ -3,3 +3,9 @@ library(haven)
 data <- read_sas('P-5262/P-5262/hs17_01.sas7bdat')
 head(data)
 
+data <- read_sas('P-5300/P-5300 (CK18-01)/pub.sas7bdat')
+head(data)
+
+data <- read_sas('P-5376/mm1802_public_dataset.sas7bdat')
+head(data)
+
